8bb82b2fb935343a027670b0e8edabfdda04cb69
lucky091588 <lucky091588@gmail.com>
2020-07-24 08:36:01 +0800

    Confirm civicrm coding standards

--- CRM/Hideactivitytypes/BAO/ActivityMask.php
+++ CRM/Hideactivitytypes/BAO/ActivityMask.php
@@ -1,6 +1,8 @@
 <?php
-use CRM_Hideactivitytypes_ExtensionUtil as E;
 
+/**
+ * use CRM_Hideactivitytypes_ExtensionUtil as E;
+ */
 class CRM_Hideactivitytypes_BAO_ActivityMask extends CRM_Hideactivitytypes_DAO_ActivityMask {
 
   /**
@@ -8,19 +10,19 @@ class CRM_Hideactivitytypes_BAO_ActivityMask extends CRM_Hideactivitytypes_DAO_A
    *
    * @param array $params key-value pairs
    * @return CRM_Hideactivitytypes_DAO_ActivityMask|NULL
-   *
-  public static function create($params) {
-    $className = 'CRM_Hideactivitytypes_DAO_ActivityMask';
-    $entityName = 'ActivityMask';
-    $hook = empty($params['id']) ? 'create' : 'edit';
+   */
+  // public static function create($params) {
+  //   $className = 'CRM_Hideactivitytypes_DAO_ActivityMask';
+  //   $entityName = 'ActivityMask';
+  //   $hook = empty($params['id']) ? 'create' : 'edit';
 
-    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
-    $instance = new $className();
-    $instance->copyValues($params);
-    $instance->save();
-    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
+  //   CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
+  //   $instance = new $className();
+  //   $instance->copyValues($params);
+  //   $instance->save();
+  //   CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
 
-    return $instance;
-  } */
+  //   return $instance;
+  // } */
 
 }
