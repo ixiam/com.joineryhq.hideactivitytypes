ed550301427c89b6a24dad13ce210130c9e44040
Lucky <lucky091588@gmail.com>
2021-02-19 23:15:48 +0800

    Fix bug: hide activity type when check in option

--- js/hideActions.js
+++ js/hideActions.js
@@ -1,7 +1,7 @@
 CRM.$(function($) {
   $('.crm-contact_activities-list').children().children().each(function( index ) {
     var id = $(this).attr('class').split('_')[1];
-    if ($.inArray(id, CRM.vars.hideActions) > -1) {
+    if ($.inArray(parseInt(id), CRM.vars.hideActions) > -1) {
       $(this).hide();
     }
   });
--- js/hideActivities.js
+++ js/hideActivities.js
@@ -3,7 +3,7 @@ CRM.$(function($) {
     $(document).ajaxSend(function() {
       $('.action-link.crm-activityLinks select').children().each(function( index ) {
         var id = $(this).attr('value').split('atype=')[1];
-        if ($.inArray(id, CRM.vars.hideActivities) > -1) {
+        if ($.inArray(parseInt(id), CRM.vars.hideActivities) > -1) {
           $(this).remove();
         }
       });
